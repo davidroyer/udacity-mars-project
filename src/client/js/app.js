876cd6657c807a568c49386a9adc0f45450dd7b7
David Royer <droyer01@gmail.com>
2020-11-25 23:40:13 -0500

    fix: fixes using wrong variable name

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,6 +5,7 @@ import { fromJS } from 'immutable';
 import Header from './components/Header/Index';
 import Main from './components/Main/Index';
 import { getStateJS, updateState } from './store';
+
 const siteURL = process.env.NODE_ENV === 'production' ? 'https://mars-rovers-dashboard.herokuapp.com/' : 'http://localhost:4000';
 
 const root = document.getElementById('root');
@@ -52,7 +53,7 @@ root.addEventListener('change', async function (event) {
 
 // eslint-disable-next-line no-unused-vars
 const getRoverData = async (roverName) => {
-  const data = await fetch(`${siteUrl}/rovers?name=${roverName}`);
+  const data = await fetch(`${siteURL}/rovers?name=${roverName}`);
   const { latest_photos } = await data.json();
   const { rover } = latest_photos[0];
   const photos = latest_photos.map((obj) => obj.img_src);
