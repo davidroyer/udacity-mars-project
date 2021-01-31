fded9c8f55ac70b55483afc65039989f6dcec837
David Royer <droyer01@gmail.com>
2021-01-30 19:01:02 -0500

    fix: adds help text when rover has not been selected

--- src/client/js/components/Main/Index.js
+++ src/client/js/components/Main/Index.js
@@ -2,7 +2,14 @@ import RoverImagesSection from './RoverImagesSection';
 import RoverInfoSection from './RoverInfoSection';
 
 const Main = (selectedRover) => {
-  if (!selectedRover.name) return `<main class="pb-16 mt-6 mb-6">`;
+  if (!selectedRover.name)
+    return `
+  <main class="pb-16 mt-6 mb-6 flex h-full items-center justify-center">
+  <h2 class="text-4xl text-indigo-900 font-light">
+  	Select a rover in the menu at top of the page.
+  </h2>
+  </main>
+  `;
 
   return `
     <main class="pb-16 mt-6 mb-6">
